17f192d7cdaacaa13b4290d766d3adf55869d71f
Nishank1705 <nishothers@gmail.com>
2025-03-20 15:09:28 +0530

    [Nishank Bathri] add. Change Port Number

---
+++ Spring_Cloud-SayHelloWorld_API/cloud-funtion-example/src/test/java/com/example/funtions/CloudFuntionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funtions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFuntionExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
